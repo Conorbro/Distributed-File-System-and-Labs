9dbfe36b2578964d86265602e4196d2ee3ef0322
Conor Broderick <cjayjayb@gmail.com>
2015-10-01 21:43:22 +0100

    first commit

---
+++ Lab1/client.rb
@@ -0,0 +1,17 @@
+# Conor Broderick - 11349681
+# Distributed Systems - CS4032
+
+require 'socket'
+
+host = 'localhost'
+port = 8000
+protocol = "HTTP/1.0\r\n\r\n"
+
+puts "Enter string to send to server (echo.php)..."
+input = gets
+request = "GET /echo.php?message=" + input + protocol
+
+socket = TCPSocket.open(host, port) # Open socket connection to port 8000 of localhost using TCP socket
+socket.write(request) # Send request
+print socket.read # Read response from server (echo.php)
+socket.close # Close the socket
